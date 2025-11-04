3bc36001a5b27aa1a296179c7c38bdf227e3fbc7
mutasim99 <virginmr882@gmail.com>
2025-11-04 08:37:24 +0600

    add reduce method

--- reduce.js
+++ reduce.js
@@ -14,7 +14,20 @@ const surveyResponses = [
     "C",
     "B",
     "A",
-  ];
-  
-  //? Output
-  // { A: 5, C: 3, B: 5, D: 1 }
+];
+
+//? Output
+// { A: 5, C: 3, B: 5, D: 1 }
+
+const count = surveyResponses.reduce((table, response)=>{
+    //* console.log(table ,":", response);
+    if (table[response]) {
+        table[response] = table[response] + 1
+    }else{
+        table[response] = 1
+    }
+
+    return table
+},{})
+
+console.log('Final output',count);
